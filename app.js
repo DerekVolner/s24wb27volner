4f840bd1f607d2404a50c594d9fcbe7767773d7f
Derek Volner <89471429+DerekVolner@users.noreply.github.com>
2024-03-26 16:29:49 -0500

    Part 9 steps 1-8 Completed

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var rhinoRouter = require('./routes/rhino');
 var gridRouter = require('./routes/grid')
+var pickRandRouter = require('./routes/pick')
 
 
 
@@ -28,6 +29,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/rhino', rhinoRouter);
 app.use('/grid', gridRouter);
+app.use('/pick',pickRandRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
